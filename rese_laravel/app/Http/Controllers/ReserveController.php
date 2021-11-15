518c9058d9fe3d4c2846ca75de42ff16d7dd3bd0
AkiyoshiSatoshi <akiyoshiakiyoshi3104@gmail.com>
2021-11-16 08:10:04 +0900

    Add :Controller file

--- rese_laravel/app/Http/Controllers/ReserveController.php
+++ rese_laravel/app/Http/Controllers/ReserveController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Reserve;
 
 class ReserveController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        return $request->all();
+    }
 }
