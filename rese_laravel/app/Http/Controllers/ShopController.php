ea79b09ebb16a75c91d0f6a218ee2da3b9335c62
AkiyoshiSatoshi <akiyoshiakiyoshi3104@gmail.com>
2021-11-18 11:04:59 +0900

    Add : Like Display Function

--- rese_laravel/app/Http/Controllers/ShopController.php
+++ rese_laravel/app/Http/Controllers/ShopController.php
@@ -6,15 +6,29 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Shop;
+use App\Models\Like;
 
 class ShopController extends Controller
 {
     public function index()
     {
         $tests = Shop::all();
-        $item = Shop::with('genres')->get();
+        $id = Auth::user()->id;
+        $search = Shop::doesnthave('likes')->get();
+        $likes=array();
+        $likes[0]='dummy';
+        foreach($tests as $shop)
+        {
+            $like=Like::where('user_id',$id)->where('shop_id',$shop['id'])->first();
+            if(!empty($like)){
+                $like=1;
+            }else{
+                $like=0;
+            }
+            array_push($likes,$like);
+        }
         if (Auth::check()) {
-            return view('shop.index', compact('tests'));
+            return view('shop.index', compact('tests', 'likes'));
         } else {
             return view('auth/login');
         }
@@ -23,5 +37,10 @@ class ShopController extends Controller
     {
         $tests = Shop::where('id', $id)->get();
         return view('shop.detail',compact('tests'));
+    }
+
+    public function mypage(Request $request)
+    {
+        
     }
 }
