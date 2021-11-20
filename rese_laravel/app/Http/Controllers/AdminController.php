d429defe23dbe1802a73bab673be39f6983bd9d4
AkiyoshiSatoshi <akiyoshiakiyoshi3104@gmail.com>
2021-11-20 12:27:48 +0900

    Fix:function / show favorites shop & reservedShop

--- rese_laravel/app/Http/Controllers/AdminController.php
+++ rese_laravel/app/Http/Controllers/AdminController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Reservation;
+use App\Models\Like;
+use App\Models\Shop;
+use App\Models\Test;
 
 class AdminController extends Controller
 {
@@ -16,11 +20,30 @@ class AdminController extends Controller
     public function adminedit()
     {
         $test = Auth::user()->access_auth;
-        // return $test;
         if ($test == 9) {
             return view('auth.login');
         } else {
             return view('shop');
         }
+    }
+
+    public function mypage()
+    {
+        $shops = Shop::all();
+        $likes=[];
+        foreach($shops as $shop)
+        {
+            $like=Like::where('user_id',Auth::user()->id)->where('shop_id',$shop['id'])->first();
+            array_push($likes,$like);
+        }
+
+        // dd($likes);
+
+        $reservations = Reservation::where('user_id', Auth::user()->id)->get();
+        // foreach ($reservations as $reservation) {
+        //     $date = date('Y/m/d', strtotime($reservation->start_at));
+        //     $time = date('H:i:s', strtotime($reservation->start_at));
+        // }
+        return view('mypage', compact('likes', 'reservations',));
     }
 }
