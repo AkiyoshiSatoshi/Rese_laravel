baa7a75985022cf9b19176f75ad0784fc4a4ec6e
AkiyoshiSatoshi <akiyoshiakiyoshi3104@gmail.com>
2021-11-22 16:18:17 +0900

    Add :: 😄権限ページ

---
+++ rese_laravel/resources/views/admin/repre.blade.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+  <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+</head>
+<body>
+  <header>
+
+  </header>
+  <main>
+    <div class="shop__create">
+      <form action="post">
+
+      </form>
+    </div>
+    <div class="shop__update">
+      <form action="post">
+
+      </form>
+    </div>
+    <div class="book__confirm">
+      
+    </div>
+  </main>
+  <footer>
+
+  </footer>
+</body>
+</html>
+
+
+
+{{-- 店舗代表者が店舗情報の作成、更新と予約情報の確認ができる管理画面があるか --}}
