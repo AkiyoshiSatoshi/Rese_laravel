5150ea92225117f0187c5c959d6eccf833b961e0
AkiyoshiSatoshi <akiyoshiakiyoshi3104@gmail.com>
2021-11-24 23:28:07 +0900

    Add function 管理画面

--- rese_laravel/app/Http/Controllers/ShopController.php
+++ rese_laravel/app/Http/Controllers/ShopController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Shop;
 use App\Models\Like;
+use App\Models\Reservation;
 
 class ShopController extends Controller
 {
@@ -36,22 +37,52 @@ class ShopController extends Controller
             return view('auth/login');
         }
     }
-    public function postshop()
+    public function postshop(Request $request)
     {
+        $image_path = $request->file('img_url')->store('public/test');
+
         $shop = new Shop;
+        $param = [
+            "name" => $request->shop_name,
+            "img_url" => $image_path,
+            "description" => $request->description,
+            "area_id" => $request->area,
+            "genre_id" => $request->genre,
+            "owner_id" => Auth::id()
+        ];
+        $shop->fill($param)->save();
+        
+        $test = Shop::all();
+        
         $adminRepre = Auth::user()->access_auth;
         if ( $adminRepre == 1 ) {
             echo "authentication success";
             dd($shop);
         } else {
             return "Not access";
-            // return view('/');
         }
+    }
+
+    public function updateshop(Request $request)
+    {
+        $shop = Shop::where('owner_id', $request->owner_id)->first();
+        $form = $request->all();
+        unset($form['_token']);
+        $shop->fill($form)->save();
+        dd($shop);
     }
 
     public function viewReps()
     {
-        return view('admin.repre');
+
+        $test = Shop::where('owner_id', Auth::id())->first();
+        // dd($test);
+        if ($test) {
+            $reservation = Reservation::where('shop_id', $test->id)->get();
+            return view('admin.repre',compact('test', 'reservation'));
+        } else {
+            return view('admin.repre');
+        }
     }
     public function shopdetail($id)
     {
