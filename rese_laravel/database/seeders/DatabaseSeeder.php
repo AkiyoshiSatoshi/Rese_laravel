fe9a3b245ce8fd66ba8671e38ae8c0d545bf9453
AkiyoshiSatoshi <akiyoshiakiyoshi3104@gmail.com>
2021-11-25 15:54:20 +0900

    Add : 3権限のダミーデータ挿入

--- rese_laravel/database/seeders/DatabaseSeeder.php
+++ rese_laravel/database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            UserTableSeeder::class,
             AreaTableSeeder::class,
             GenreTableSeeder::class,
             ShopTableSeeder::class,
---
+++ rese_laravel/database/seeders/UserTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('admin1234'),
+            'access_auth' => 1,
+        ]);
+    }
+}
