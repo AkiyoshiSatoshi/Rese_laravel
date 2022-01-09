4be192be142e0a8d9ec61ba82eaacf8122c044b5
AkiyoshiSatoshi <akiyoshiakiyoshi3104@gmail.com>
2022-01-09 15:00:49 +0900

    予約完了時のthanksページ作成

--- rese_laravel/app/Http/Controllers/ShopController.php
+++ rese_laravel/app/Http/Controllers/ShopController.php
@@ -61,4 +61,26 @@ class ShopController extends Controller
         $shops = Shop::where('id', $id)->get();
         return view('shop.detail',compact('shops'));
     }
+
+
+    public function shopsearch(Request $request)
+    {
+        $name = $request->name;
+        $area = $request->area;
+        $genre = $request->genre;
+
+        if ($area == 0 && $genre == 0 ) {
+            $shops = Shop::where('name','like','%'.$name.'%')->get();
+            return view('shop.index',compact('shops'));
+        } else if (is_null($name) && !is_null($area) && $genre == 0) {
+            $shops = Shop::where('area_id', $area)->get();
+            return view('shop.index',compact('shops'));
+        } else if (is_null($name) && $area == 0 && !is_null($genre)) {
+            $shops = Shop::where('genre_id', $genre)->get();
+            return view('shop.index',compact('shops'));
+        } else {
+            $shops = Shop::all();
+            return view('shop.index',compact('shops'));
+        }
+    }
 }
---
+++ rese_laravel/resources/views/user/thanks.blade.php
@@ -0,0 +1,18 @@
+<div class="thank__container">
+  <h1>ご予約ありがとうございます</h1>
+  <a href="/shop">戻る</a>
+</div>
+
+<style>
+.thank__container {
+  border-radius: 0px;
+  background: #ffffff;
+  box-shadow:  3px 3px 6px #898989,
+              -3px -3px 6px #ffffff;
+  text-align: center;
+  width: 50%;
+  margin: 200px auto;
+  padding: 100px 0;
+}
+
+</style>
