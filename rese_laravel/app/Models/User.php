a6567bf4a0bdfa6fa7d7be8b2cda98506b9eda9d
AkiyoshiSatoshi <akiyoshiakiyoshi3104@gmail.com>
2022-05-09 15:15:46 +0900

    Add : Stripe environment setting

--- rese_laravel/app/Models/User.php
+++ rese_laravel/app/Models/User.php
@@ -9,14 +9,18 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
+use Laravel\Cashier\Billable;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
+    use Billable;
     use HasApiTokens;
     use HasFactory;
     use HasProfilePhoto;
     use Notifiable;
     use TwoFactorAuthenticatable;
+
+
     public function sendEmailVerificationNotification()
     {
         $this->notify(new \App\Notifications\VerifyEmail);
