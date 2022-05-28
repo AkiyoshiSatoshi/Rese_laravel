c65b8416b0d4d9ba2195a25e272417fe67d73a26
AkiyoshiSatoshi <akiyoshiakiyoshi3104@gmail.com>
2022-05-28 09:42:30 +0900

    change: stripe Controller

--- rese_laravel/app/Http/Controllers/StripeController.php
+++ rese_laravel/app/Http/Controllers/StripeController.php
@@ -11,53 +11,14 @@ class StripeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function viewStripe()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function postStripe()
     {
-        //
+        # code...
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
